d2bfa1305f601c1798ba9d13e4f36c84f9a88251
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-04-17 16:12:16 +0530

    issue

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='ecommerce'>
       <Provider store={store}>
         <App />
       </Provider>
